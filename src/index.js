7c76381a30595b4298772ea843ecbd1e4073a360
Olegexe <43041704+Olegexe@users.noreply.github.com>
2021-07-13 12:24:45 +0300

    Created an icon font. Added the remanining animations. Added Smooth Scroll

--- src/index.js
+++ src/index.js
@@ -4,5 +4,22 @@ import 'aos/dist/aos.css';
 
 AOS.init({
    once: true,
-   duration: 800,
+   duration: 1000,
 });
+
+SmoothScroll({
+   animationTime    : 1000,
+   stepSize         : 100,
+   accelerationDelta : 30,  
+   accelerationMax   : 2,   
+
+   keyboardSupport   : true,  
+
+   arrowScroll       : 50,
+
+   pulseAlgorithm   : true,
+   pulseScale       : 4,
+   pulseNormalize   : 1,
+
+   touchpadSupport   : true,
+})
